573e4ecc859a3f625c9bc86c052f83c9d143c445
unknown <ankit.parashar@digimantra.com>
2021-03-06 02:20:51 +0530

    minor change

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Routes from './routes/Routes';
-import $ from 'jquery';
-import Popper from 'popper.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
